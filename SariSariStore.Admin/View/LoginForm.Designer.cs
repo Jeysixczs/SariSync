62c23d8c56c47f6cc786f4e52abdb2f7713ce7b2
Jeysixczs <johncarloaquino205@gmail.com>
2025-11-17 23:35:29 +0800

    Remove Bypass Login

--- SariSariStore.Admin/View/LoginForm.Designer.cs
+++ SariSariStore.Admin/View/LoginForm.Designer.cs
@@ -101,13 +101,8 @@
             // 
             // button2
             // 
-            button2.Location = new Point(135, 397);
-            button2.Name = "button2";
-            button2.Size = new Size(75, 23);
-            button2.TabIndex = 5;
-            button2.Text = "button2";
-            button2.UseVisualStyleBackColor = true;
-            button2.Click += button2_Click_1;
+           
+         
             // 
             // label5
             // 
